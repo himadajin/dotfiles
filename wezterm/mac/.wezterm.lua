6f3ddeb5159cf42cc2aeee75fc0750e2626849da
HARADA Taiki <himadajin@gmail.com>
2026-07-20 11:26:17 +0900

    fix: update .wezterm.lua

--- wezterm/mac/.wezterm.lua
+++ wezterm/mac/.wezterm.lua
@@ -7,12 +7,14 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Configrations
+config.default_prog = { "/bin/zsh", "-l" }
+
 -- Colors & Appearance
-config.color_scheme = "Tokyo Night (Gogh)"
+config.color_scheme = "One Half Black (Gogh)"
 config.window_background_opacity = 0.97
 config.default_cursor_style = "SteadyBar"
 config.use_ime = true
@@ -20,45 +22,45 @@ config.audible_bell = "Disabled"
 
 -- Launching Programs
 config.launch_menu = {
-	{
-		args = { "top" },
-	},
-	{
-		label = "Docker: debian:bullseye",
-		args = { "/usr/local/bin/docker", "run", "-it", "debian:bullseye" },
-	},
+  {
+    args = { "top" },
+  },
+  {
+    label = "Docker: debian:bullseye",
+    args = { "/usr/local/bin/docker", "run", "-it", "debian:bullseye" },
+  },
 }
 
 -- Key binding
 local act = wezterm.action
 config.keys = {
-	-- Pane
-	{
-		key = "s",
-		mods = "SHIFT|CMD",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "d",
-		mods = "SHIFT|CMD",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "w",
-		mods = "CMD",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	-- Tab
-	{
-		key = "{",
-		mods = "SHIFT|CMD",
-		action = act.MoveTabRelative(-1),
-	},
-	{
-		key = "}",
-		mods = "SHIFT|CMD",
-		action = act.MoveTabRelative(1),
-	},
+  -- Pane
+  {
+    key = "s",
+    mods = "SHIFT|CMD",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "d",
+    mods = "SHIFT|CMD",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "w",
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentPane({ confirm = true }),
+  },
+  -- Tab
+  {
+    key = "{",
+    mods = "SHIFT|CMD",
+    action = act.MoveTabRelative(-1),
+  },
+  {
+    key = "}",
+    mods = "SHIFT|CMD",
+    action = act.MoveTabRelative(1),
+  },
 }
 
 return config
